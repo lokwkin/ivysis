8a3e95220179321c92a47244ce50e1e3f6a7c5f8
lokwkin <lokwkin@gmail.com>
2025-02-02 23:47:02 +0000

    storing memos generated

--- memoboard/memoboard_builder.py
+++ memoboard/memoboard_builder.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from typing import List, Literal
+from uuid import uuid4
 from pydantic import BaseModel
 from common.logger import get_logger
+from common.utils import safe_write_file
 from data_loader.base_email_fetcher import EmailMessage
 from llm.clients.base_llm_client import BaseLLMClient
 from llm.templates.message_digest.information_extraction import InformationExtractionPrompt
@@ -19,16 +21,18 @@ class SourceMessage(BaseModel):
 class Memo(BaseModel):
     type: Literal["actionable", "informative"]
     source: SourceMessage
+    categories: List[str]
     details: str
     summary: str
 
 
 class MemoboardBuilder:
-    def __init__(self, llm_client: BaseLLMClient, persona: str):
+    def __init__(self, llm_client: BaseLLMClient, persona: str, storage_path: str):
         self.llm_client = llm_client
         self.persona = persona
+        self._storage_path = f"{storage_path}/memoboard"
 
-    def read_email(self, email: EmailMessage) -> List[Memo]:
+    def process_email(self, email: EmailMessage) -> List[Memo]:
         summary_result = self.llm_client.prompt(
             template=EmailSummarizingPrompt,
             template_params={
@@ -51,12 +55,22 @@ class MemoboardBuilder:
                 "summary": summary_result.summary,
             }
         )
-        return [
-            Memo(
+
+        memos: List[Memo] = []
+        for extraction in extraction_result.extractions:
+            memo = Memo(
                 type=extraction.type,
                 source=SourceMessage(date=email.date, summary=summary_result.summary),
+                categories=extraction.categories,
                 details=extraction.details,
                 summary=summary_result.summary,
             )
-            for extraction in extraction_result.extractions
-        ]
+            self._save_memo(memo)
+            memos.append(memo)
+        return memos
+
+    def _save_memo(self, memo: Memo):
+        for category in memo.categories:
+            uuid = str(uuid4())
+            file_path = f"{self._storage_path}/{category}/{str(uuid)[:8]}.json"
+            safe_write_file(file_path, memo.model_dump_json())
